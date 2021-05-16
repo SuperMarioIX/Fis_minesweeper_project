97aad6fca628ec8c6e6d6c4b67ab7d66c5a625ab
Frunza Marius <marius.frunza.2001@gmail.com>
2021-05-16 22:11:08 +0300

    MINF-17 Engine Pagina pentru administrator

---
+++ src/main/java/controllers/AdminHomeScreen.java
@@ -0,0 +1,46 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.util.Objects;
+
+public class AdminHomeScreen {
+    @FXML
+    public void back(ActionEvent event) throws IOException {
+        Parent viewParent = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("fxmlFiles/MainPage.fxml")));
+        Scene viewScene = new Scene(viewParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(viewScene);
+        window.show();
+    }
+    @FXML
+    public void Leaderboard(ActionEvent event) throws IOException {
+        Parent viewParent = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("fxmlFiles/Leaderboardadmin.fxml")));
+        Scene viewScene = new Scene(viewParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(viewScene);
+        window.show();
+    }
+    @FXML
+    public void Easy (){
+        new GamePlay();
+    }
+
+    @FXML
+    public void Medium (){
+        new GamePlay1();
+    }
+    @FXML
+    public void Expert (){
+        new GamePlay2();
+    }
+}
